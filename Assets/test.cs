9f25667d52aafb3a6f85cf18bedf67b1edf43423
tomoya.oono <tomoya00060107@gmail.com>
2018-03-31 16:43:37 +0900

    scripttest 2回目のコミット 課題の訂正

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,49 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; // mp
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+    // 魔法攻撃用の関数
+    public void magic(int cost)
+    {
+        if (mp > cost)
+        {
+            // 残りmpが消費mp以上だった場合
+            mp -= cost;
+            Debug.Log("魔法攻撃をした。残りMPは"+ mp);
+        }
+        else
+        {
+            //残りmpが消費mp未満だった場合
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+}
 
 public class test : MonoBehaviour
 {
 
-
-    // mpから消費mpを引いた値を返す関数
-    int magic(int a)
-    {
-        // mpから消費mpを引いて、変数bに代入する
-        int b = a - 5;
-        // 変数bを呼び出し元の関数に返す
-        return b;
-    }
-
-    // Use this for initialization
     void Start()
     {
+        // 配列を初期化する
+        int[] array = { 30, 20, 50, 10, 80, };
 
-        //変数の宣言
-        int mp;
-        //変数の代入
-        mp = 53;
+        // 配列の要素数のぶんだけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++)
+        {
+                // 配列の要素を表示する
+                Debug.Log(array[i]);
+        }
 
+        // 配列の各要素に値を代入する
+        array[0] = 80;
+        array[1] = 10;
+        array[2] = 50;
+        array[3] = 20;
+        array[4] = 30;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            // 配列の要素を表示する
+            Debug.Log(array[i]);
+        }
+
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        //魔法攻撃用の関数を呼び出す
         for (int i = 0; i < 11; i++)
         {
-
-            // magic関数に残りmp数を渡し、返り値をnum変数に代入する
-            int num = magic(mp);
-
-            if (mp < 5)
-            {
-                // mpが5未満だった場合
-                Debug.Log("MPが足りないため魔法が使えない。");
-            }
-            else
-            {
-                // mpが5以上だった場合残りmpをmpに代入し、numを表示する
-                mp = num;
-                Debug.Log("魔法攻撃をした。残りMPは" + num);
-            }
+            lastboss.magic(5);
         }
     }
-
 
     // Update is called once per frame
     void Update()
